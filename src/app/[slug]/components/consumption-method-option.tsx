5eba041e4d15ee276a67674140d5b5952dc2f824
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2025-04-10 17:58:41 -0300

    feat: add_slug_routerType

---
+++ src/app/[slug]/components/consumption-method-option.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { ConsumptionMethod } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ConsumptionMethodOptionProps {
+  slug: string;
+  imageUrl: string;
+  imageAlt: string;
+  buttonText: string;
+  option: ConsumptionMethod;
+}
+
+const ConsumptionMethodOption = ({
+  slug,
+  imageAlt,
+  imageUrl,
+  buttonText,
+  option,
+}: ConsumptionMethodOptionProps) => {
+  return (
+    <Card>
+      <CardContent className="flex flex-col items-center gap-8 py-8">
+        <Link
+          href={`/${slug}/menu?consumptionMethod=${option}`}
+          className="flex flex-col items-center"
+        >
+          <div className="relative h-[80px] w-[80px]">
+            <Image
+              src={imageUrl}
+              fill
+              alt={imageAlt}
+              className="object-contain"
+            />
+          </div>
+          <Button variant="secondary" className="rounded-full">
+            {buttonText}
+          </Button>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ConsumptionMethodOption;
---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,36 @@
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await getRestaurantBySlug(slug);
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Image src={restaurant?.coverImageUrl} alt={restaurant.name} fill />
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
