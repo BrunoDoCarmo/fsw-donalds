eeff705be32f2379e60806600c216a49b1301875
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2025-04-14 17:51:20 -0300

    feat: começando a criar a pagina de produto

--- src/data/get-restaurant-by-slug.ts
+++ src/data/get-restaurant-by-slug.ts
@@ -1,14 +1,6 @@
-import { Prisma } from "@prisma/client";
-
 import { db } from "@/lib/prisma";
 
-export const getRestaurantBySlug = async (
-  slug: string,
-  options?: Omit<Prisma.RestaurantFindUniqueArgs, "where">,
-) => {
-  const restaurant = await db.restaurant.findUnique({
-    where: { slug },
-    ...options,
-  });
+export const getRestaurantBySlug = async (slug: string) => {
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
   return restaurant;
 };
