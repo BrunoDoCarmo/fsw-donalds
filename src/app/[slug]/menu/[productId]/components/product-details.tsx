8bdf063a3cde1348b0a6ced800b1945a0cd98f13
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2025-04-14 22:29:16 -0300

    feat: add_product_details_in_process

---
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -0,0 +1,79 @@
+"use client" 
+
+import { Prisma } from "@prisma/client";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/format-currency";
+
+interface ProductDetailsProps {
+    product: Prisma.ProductGetPayload<{
+        include: {
+            restaurant: {
+                select: {
+                    name: true,
+                    avatarImageUrl: true
+                }
+            }
+        }
+    }>
+}
+
+const ProductDetails = ({ product }: ProductDetailsProps) => {
+    const [quantity, setQuantity] = useState<number>(1);
+    const handleDecreaseQuantity = () => {
+        setQuantity((prev) => {
+            if (prev === 1) {
+                return 1;
+            }
+            return prev - 1;
+        });
+    };
+    const handleIncreaseQuantity = () => {
+        setQuantity((prev) => prev + 1);
+    };
+    return ( 
+        <>
+            <div className="relative z-50 mt-[1.5rem] flex flex-auto flex-col overflow-hidden rounded-t-3xl p-5">
+                <div className="flex-auto overflow-hidden">
+                    {/* RESTAURANTE */}
+                    <div className="flex items-center gap-1.5">
+                        <Image
+                            src={product.restaurant.avatarImageUrl}
+                            alt={product.restaurant.name}
+                            width={16}
+                            height={16}
+                            className="rounded-full"
+                        />
+                        <p className="text-xs text-muted-foreground">
+                            {product.restaurant.name}
+                        </p>
+                    </div>
+                    
+                    {/* NOME DO PRODUTO */}
+                    <h2 className="text-xl mt-1 font-semibold">{product.name}</h2>
+
+                    {/* PREÇO E QUANTIDADE */}
+                    <div className="mt-3 flex items-center justify-between">
+                        <h3 className="text-xl font-semibold">
+                            {formatCurrency(product.price)}
+                        </h3>
+                        <div className="flex items-center gap-3 text-center">
+                            <Button variant="outline" className="h-8 w-8 rounded-xl" onClick={handleDecreaseQuantity}>
+                                <ChevronLeftIcon/>                                
+                            </Button>
+                            <p className="w-4">{quantity}</p>
+                            <Button variant="destructive" className="h-8 w-8 rounded-xl" onClick={handleIncreaseQuantity}>
+                                <ChevronRightIcon/>                                
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+     );
+}
+ 
+export default ProductDetails;
--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import ProductDetails from "./components/product-details";
 import ProductHeader from "./components/product-header";
 
 
@@ -11,7 +12,7 @@ interface ProductPageProps {
 
 const ProductPage = async ({ params }: ProductPageProps) => {
   const { slug, productId } = await params;
-  const product = await db.product.findUnique({where: { id: productId}})
+  const product = await db.product.findUnique({where: { id: productId}, include: {restaurant: true}})
 
   if(!product) {
     return notFound()
@@ -20,6 +21,7 @@ const ProductPage = async ({ params }: ProductPageProps) => {
   return (
     <>
       <ProductHeader product={product} />
+      <ProductDetails product={product} />
         <h1>Product Page</h1>
         {slug}
         {productId}
